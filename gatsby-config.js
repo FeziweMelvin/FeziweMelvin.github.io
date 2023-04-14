7dcef051ca9960dc8c07118f31a67904f54f49c5
Feziwe Melvin <melvin.shongwe@gmail.com>
2023-04-14 13:28:22 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Feziwe Shongwe',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Data Science and Cloud Computing Enthusiast',
+    siteUrl: 'https://feziweshongwe.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@FeziweMelvin',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Feziwe Shongwe',
+        short_name: 'Feziwe Shongwe',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
