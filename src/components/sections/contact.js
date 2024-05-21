5272f213d38030e22f621f19346ed0641f8b9d4c
Feziwe Melvin <melvin.shongwe@gmail.com>
2024-05-21 09:16:37 +0200

    contact

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,10 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+      While I'm currently engaged in my current role, 
+      I'm actively exploring opportunities within the dynamic landscape of the tech industry. 
+      Feel free to reach out with any inquiries or just to connect.
+       I'll make sure to respond promptly!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
